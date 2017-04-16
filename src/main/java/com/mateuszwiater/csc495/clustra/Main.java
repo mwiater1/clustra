e7b5fcfb4a13e5d84b006a2bc76523076d215682
Mateusz Wiater <mateuszwiater@gmail.com>
2017-04-16 01:07:04 -0400

    WIP

---
+++ src/main/java/com/mateuszwiater/csc495/clustra/Main.java
@@ -0,0 +1,25 @@
+package com.mateuszwiater.csc495.clustra;
+
+import com.pi4j.io.gpio.*;
+
+import static com.pi4j.io.gpio.PinState.HIGH;
+import static com.pi4j.io.gpio.PinState.LOW;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello World!");
+
+        final GpioController gpio = GpioFactory.getInstance();
+
+        final GpioPinDigitalOutput pin1 = gpio.provisionDigitalOutputPin(OrangePiPin.GPIO_01, LOW);
+        final GpioPinDigitalOutput pin2 = gpio.provisionDigitalOutputPin(OrangePiPin.GPIO_01, HIGH);
+
+        while (true) {
+            pin1.toggle();
+            pin2.toggle();
+            System.out.println("Toggle");
+            Thread.sleep(1000);
+        }
+    }
+}
