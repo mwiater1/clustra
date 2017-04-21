6a8244e4b01609be0050d2f996d0d8f8291615b7
Mateusz Wiater <mateuszwiater@gmail.com>
2017-04-21 02:33:08 -0400

    WIP

--- src/main/java/com/mateuszwiater/csc495/clustra/Main.java
+++ src/main/java/com/mateuszwiater/csc495/clustra/Main.java
@@ -1,25 +1,40 @@
 package com.mateuszwiater.csc495.clustra;
 
 import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.pi4j.platform.Platform;
+import com.pi4j.platform.PlatformAlreadyAssignedException;
+import com.pi4j.platform.PlatformManager;
 
-import static com.pi4j.io.gpio.PinState.HIGH;
-import static com.pi4j.io.gpio.PinState.LOW;
+import java.util.Arrays;
+
+import static com.pi4j.io.gpio.OrangePiPin.*;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello World!");
-
+    public static void main(String[] args) throws InterruptedException, PlatformAlreadyAssignedException {
+        PlatformManager.setPlatform(Platform.ORANGEPI);
+//        GpioFactory.setDefaultProvider(new OrangePiGpioProvider());
         final GpioController gpio = GpioFactory.getInstance();
 
-        final GpioPinDigitalOutput pin1 = gpio.provisionDigitalOutputPin(OrangePiPin.GPIO_01, LOW);
-        final GpioPinDigitalOutput pin2 = gpio.provisionDigitalOutputPin(OrangePiPin.GPIO_01, HIGH);
+        Arrays.asList(GPIO_00, GPIO_01, GPIO_02, GPIO_03, GPIO_04, GPIO_05, GPIO_11, GPIO_12, GPIO_13, GPIO_15, GPIO_16, GPIO_07).stream()
+                .map(p -> gpio.provisionDigitalInputPin(p, PinPullResistance.PULL_DOWN))
+                .forEach(p -> {
+                            System.out.println("Adding: " + p.getName());
+                    try {
+                        p.addListener((GpioPinListenerDigital) event ->
+                                System.out.println("GPIO Pin '" + event.getPin().getName() + "' is: " + event.getState())
+                        );
+                    } catch (Exception e) {
+                        System.out.println("Exception on Pin: " + p.getName());
+                        System.out.println(e.toString());
+                    }
+                }
+                );
 
-        while (true) {
-            pin1.toggle();
-            pin2.toggle();
-            System.out.println("Toggle");
-            Thread.sleep(1000);
+        System.out.println("Ready, waiting...");
+        while(true) {
+            Thread.sleep(500);
         }
     }
 }
